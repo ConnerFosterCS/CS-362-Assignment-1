81078f3eb21a2c4982a75fefa5a8776f4bac2383
ConnerFosterCS <61959809+ConnerFosterCS@users.noreply.github.com>
2021-03-07 22:20:08 -0800

    Wrote Second Test
    Added unittest to check the numbers divisible by both 5 and 3 and making sure that they print "FizzBuzz" instead of the number, "Fizz", or "Buzz".

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
 # cs362 section 001
 # FizzBuzz
 def fizzbuzz(num): 
-	return num
+  if ((num % 3) == 0 and (num % 5) == 0):
+    return "FizzBuzz"
+  else:
+    return num
--- tests.py
+++ tests.py
@@ -7,5 +7,10 @@ class TestCase(unittest.TestCase):
     self.assertEqual(main.fizzbuzz(52), 52)
     self.assertEqual(main.fizzbuzz(31), 31)
 
+  def testFizzBuzz(self):
+    self.assertEqual(main.fizzbuzz(30), "FizzBuzz")
+    self.assertEqual(main.fizzbuzz(60), "FizzBuzz")
+    self.assertEqual(main.fizzbuzz(90), "FizzBuzz")
+
 if __name__ == '__main__':
   unittest.main()
